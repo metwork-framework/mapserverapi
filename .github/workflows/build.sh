5864bca3c9a1085f865dba428ac33c0d0301e875
thebaptiste <jean-baptiste.veslin@meteo.fr>
2022-04-28 13:34:46 +0000

    build: fix CI

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -x
+
 yum -y install metwork-mfext-layer-mapserver
+cd /src
 /opt/metwork-mfext/bin/mfext_wrapper layer_wrapper --layers=mapserver@mfext -- make MAPSERVER_LIB_DIR=/opt/metwork-mfext/opt/mapserver/lib PREFIX=/opt/metwork-mfext/opt/mapserver clean all test install
